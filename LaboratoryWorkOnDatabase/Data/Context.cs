7974e840cbdb63f8fa31189d65c1fd0ac67fb4a9
Даниил Лямин <Niger120404@yandex.ru>
2023-11-09 15:32:25 +0300

    A context has been created for connecting and working with the database

---
+++ LaboratoryWorkOnDatabase/Data/Context.cs
@@ -0,0 +1,24 @@
+﻿using LaboratoryWorkOnDatabase.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaboratoryWorkOnDatabase.Data
+{
+    public class Context(DbContextOptions<Context> options) : DbContext(options)
+    {
+        public DbSet<Discipline> Disciplines { get; set; }
+
+        public DbSet<Form> Forms { get; set; }
+
+        public DbSet<Plan> Plans { get; set; }
+
+        public DbSet<Sheet> Sheets { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Teacher> Teachers { get; set; }
+
+        public DbSet<TeacherDiscipline> TeacherDisciplines { get; set; }
+
+        public DbSet<Title> Titles { get; set; }
+    }
+}
