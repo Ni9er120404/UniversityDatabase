7433be5d374fb99b39c914ca097b05360140d4f2
Даниил Лямин <Niger120404@yandex.ru>
2023-11-09 15:32:49 +0300

    A service for working with the database has been created

---
+++ LaboratoryWorkOnDatabase/Services/TeacherService/ITeacherService.cs
@@ -0,0 +1,17 @@
+﻿using LaboratoryWorkOnDatabase.Models;
+
+namespace LaboratoryWorkOnDatabase.Services.TeacherService
+{
+    public interface ITeacherService
+    {
+        Task<IEnumerable<Teacher>> GetAll();
+
+        Task<Teacher> Get(int id);
+
+        Task<Teacher> Create(Teacher item);
+
+        Task<Teacher> Update(Teacher item);
+
+        Task<Teacher> Delete(int id);
+    }
+}
