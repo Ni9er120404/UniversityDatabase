b422477def7fbac615cfe3d1c90dc5115dbdeacb
Даниил Лямин <Niger120404@yandex.ru>
2023-11-10 13:11:43 +0300

    A controller has been created to add test data to the database

---
+++ LaboratoryWorkOnDatabase/Controllers/DataController.cs
@@ -0,0 +1,25 @@
+﻿using LaboratoryWorkOnDatabase.Data;
+using LaboratoryWorkOnDatabase.Services.DataGenerator;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaboratoryWorkOnDatabase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController(Context context) : ControllerBase
+    {
+        private readonly Context _context = context;
+
+        [HttpPost]
+        public async Task<ActionResult> CreateData(int quantity)
+        {
+            DataGeneratorService dataGenerator = new();
+            
+            await dataGenerator.SaveDataInDatabase(_context, quantity);
+            
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
